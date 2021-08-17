b163d351be9756baf0cabbe7f652f1c2776b958b
shashank <44959680+shashank2123@users.noreply.github.com>
2021-08-17 21:30:56 +0530

    Update punctuators.py
    removing memory run out error

--- punctuators.py
+++ punctuators.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Aug 16 23:00:52 2021
+Created on Tue Aug 17 20:57:42 2021
 
 @author: shashank
 """
@@ -10,18 +10,18 @@ from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 from youtube_transcript_api import YouTubeTranscriptApi
 
 def isValidURL(str):
- 
+    
     # Regex to check valid URL
     regex = ("https://youtu.be/\w+")
      
     # Compile the ReGex
     p = re.compile(regex)
- 
+     
     # If the string is empty
     # return false
     if (str == None):
         return False
- 
+     
     # Return if the string
     # matched the ReGex
     if(re.search(p, str)):
@@ -30,78 +30,89 @@ def isValidURL(str):
         return False
 
 def predict(transcript_text):
+    PATH = "shashank2123/t5-base-fine-tuned-for-Punctuation-Restoration"
+
+    tokenizer = AutoTokenizer.from_pretrained( PATH)
+
+    token_data = tokenizer.encode(transcript_text)[:-1]
   
-  PATH = "shashank2123/t5-base-fine-tuned-for-Punctuation-Restoration"
-
-  tokenizer = AutoTokenizer.from_pretrained(PATH)
+    MAX_LEN = 256
   
-  token_data = tokenizer.encode(transcript_text)[:-1]
-
-  MAX_LEN = 256
-
-  batch_token = [token_data[i:i+MAX_LEN-1] + [1] for i in range(0 , len(token_data) , MAX_LEN-1) ]
-
-  batch_token[-1] = batch_token[-1] + [0]*(MAX_LEN - len(batch_token[-1]))
-
-  device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-  model = AutoModelForSeq2SeqLM.from_pretrained( PATH )
+    batch_token = [token_data[i:i+MAX_LEN-1] + [1] for i in range(0 , len(token_data) , MAX_LEN-1) ]
   
-  model.to(torch.device(device))
+    batch_token[-1] = batch_token[-1] + [0]*(MAX_LEN - len(batch_token[-1]))
+  
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-  ids = torch.tensor(batch_token).to(device, dtype = torch.long)
+    model = AutoModelForSeq2SeqLM.from_pretrained( PATH )
 
-  generated_ids = model.generate(
-                  input_ids = ids,
-                  max_length=256, 
-                  num_beams=2,
-                  repetition_penalty=2.5, 
-                  length_penalty=1.0, 
-                  early_stopping=True
-                  )
-
-  preds = [tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=True) for g in generated_ids]
-
-  output_text = " ".join(preds)
-
-  return output_text
-
+    model.to(torch.device(device))
+  
+    ids = torch.tensor(batch_token).to(device, dtype = torch.long)
+    batch_list = []
+    batch_size = 16
+    for i in range(0,len(batch_token),batch_size):
+        batch_list.append(batch_token[i:i+batch_size])
+  
+    pred_batch = []
+    for batch in batch_list:
+        ids = torch.tensor(batch).to(device, dtype = torch.long)
+        generated_ids = model.generate(
+                        input_ids = ids,
+                        max_length=256, 
+                        num_beams=2,
+                        repetition_penalty=2.5, 
+                        length_penalty=1.0, 
+                        early_stopping=True
+                        )
+        pred_batch.append(generated_ids)
+  
+  
+  
+    preds = [tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=True) for batch in pred_batch for g in batch]
+  
+    output_text = " ".join(preds)
+  
+    return output_text
 
 class URLError(Exception):
-	def __init__(self , message):
-		super().__init__(message)
-
-
+    def __init__(self , message):
+        super().__init__(message)
+        
 
 def get_transcript_by_url(youtube_url):
 
-  video_id = youtube_url.split('/')[-1]
+    video_id = youtube_url.split('/')[-1]
 
-  try:
-    transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
-  except:
-    raise URLError("Youtube URL is Incorrect or Video transcript is not accessible")
+    try:
+        transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
+    except:
+        raise URLError("Youtube URL is Incorrect or Video transcript is not accessible")
   
-  transcript_text = ''
-  for temp in transcript_list:
-    if 'text' in temp.keys():
-      transcript_text = transcript_text + ' '+ temp['text']
+    transcript_text = ''
+    for temp in transcript_list:
+        if 'text' in temp.keys():
+            transcript_text = transcript_text + ' '+ temp['text']
 
-  return transcript_text
+    return transcript_text
+
+
 
 if __name__ == '__main__':
-	import sys
-	args = sys.argv[1:]
+    import sys
 
-	if len(args)!=0:
-		url = args[0]
-		if isValidURL(url):
-			transcript_text = get_transcript_by_url(url)
-			print(predict(transcript_text))
+    args = sys.argv[1:]
 
-		else:
-			raise URLError("URL pattern not matched [Ex :- https://youtu.be/BrmyEWrp22A ]")
-	else:
-		raise URLError("URL not provided!")
+    if len(args)!=0:
+        url = args[0]
+        if isValidURL(url):
+            print("Getting transcript from URL : {}".format(url))
+            transcript_text = get_transcript_by_url(url)
+            print("transcript text is collected....")
+            print("Starting Inference...")
+            print(predict(transcript_text))
+        else:
+            raise URLError("URL pattern not matched [Ex :- https://youtu.be/BrmyEWrp22A ]")
 
-
+    else:
+        raise URLError("URL not provided!")
